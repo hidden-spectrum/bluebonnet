4d1debc5f21e95004766a181ef39960305d73b8a
Andrew Theis <andrewtheis@icloud.com>
2019-01-31 12:51:56 -0600

    Add initial code for defining servers and available environments

---
+++ Bluebonnet/Sources/Server.swift
@@ -0,0 +1,74 @@
+//
+//  Copyright (c) 2019 Theis Holdings, LLC.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+
+/// Represents a backend server. This is used to generate the base URLs for a `ServiceRequest`.
+///
+/// - Extend this struct with your servers:
+/// ```
+/// extension Server {
+///     static let herokuStaging = Server(host: "www.herokuapp.com/myapp-staging")
+///     static let herokuProduction = Server(host: "www.herokuapp.com/myapp")
+/// }
+/// ```
+public struct Server: Codable {
+    internal let host: String
+    
+    /// Creates a new instance of `Server` with the given host.
+    public init(host: String) {
+        self.host = host
+    }
+}
+
+extension Server: Equatable {
+    public static func == (lhs: Server, rhs: Server) -> Bool {
+        return lhs.host == rhs.host
+    }
+}
+
+extension Server {
+    /// Stores a closure that allows you to define how to map an `Environment` to defined `Server`
+    /// instance.
+    ///
+    /// - Extend this struct to define your maps:
+    /// ```
+    /// extension Server.Map {
+    ///     static let main = Server.Map { environment in
+    ///         switch environment {
+    ///             case .staging:
+    ///                 return .herokuStaging
+    ///             case .production:
+    ///                 return .herokuProduction
+    ///         }
+    ///     }
+    /// }
+    /// ```
+    public struct Map<Env: Environment> {
+        internal let block: ((Env) -> Server)
+        
+        /// Creates a new instance of `Server.Map` with the given block. See `Server.Map` for more
+        /// info.
+        public init(_ block: @escaping ((Env) -> Server)) {
+            self.block = block
+        }
+    }
+}
---
+++ Bluebonnet/Sources/Service.swift
@@ -0,0 +1,60 @@
+//
+//  Copyright (c) 2019 Theis Holdings, LLC.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+/// Represents an API service, or a collection of APIs. This can be used to point to specific
+/// versions of an  API, or different services entirely. Each `ServiceRequest` can point to a
+/// different `Service`.
+///
+/// - Use extensions to define a new `Service`:
+/// ```
+/// extension Service {
+///     static let v1 = Service(serverMap: ..., contextRoot: ...)
+/// }
+/// ```
+public struct Service<Env: Environment> {
+    
+    /// The `Server.Map` for this `Service`. See `Server.Map` for more info.
+    public let serverMap: Server.Map<Env>
+    
+    /// The context root for this service. If you have a service that uses the same `Server.Map`
+    /// then you can use this to define a path that is always present, for example `"v1"`.
+    public let contextRoot: String?
+    
+    // This generates the base URL for the current environment. Used in `ServiceRequest` to generate
+    // the URL for the request.
+    internal var baseUrlForCurrentEnvironment: URL {
+        guard let environment = Env.current else {
+            fatalError("Attempted to generate a base URL when no current environment has been set")
+        }
+        
+        var urlString = "https://" + self.serverMap.block(environment).host
+        if let contextRoot = self.contextRoot {
+            urlString += "/" + contextRoot
+        }
+        return URL(string: urlString)!
+    }
+    
+    public init(serverMap: Server.Map<Env>, contextRoot: String? = nil) {
+        self.serverMap = serverMap
+        self.contextRoot = contextRoot
+    }
+}
