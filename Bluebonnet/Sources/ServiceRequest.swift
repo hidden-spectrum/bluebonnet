f985441ef2dbae1bb9414f72563dcf810f9783f5
Andrew Theis <andrewtheis@icloud.com>
2019-02-07 00:45:48 -0600

    Fix logging for K/V pairs

--- Bluebonnet/Sources/ServiceRequest.swift
+++ Bluebonnet/Sources/ServiceRequest.swift
@@ -198,7 +198,7 @@ extension ServiceRequest {
         os_log("Request: %{public}@ %{public}@", log: .bluebonnet, type: .default, request.httpMethod ?? "<UNKNOWN HTTP METHOD>", request.debugDescription)
         
         request.allHTTPHeaderFields?.forEach { key, value in
-            os_log("\t%{public}: %@", log: .bluebonnet, type: .default, key, value)
+            os_log("%{public}@: %@", log: .bluebonnet, type: .default, key, value)
         }
         
         if let response = response as? HTTPURLResponse {
