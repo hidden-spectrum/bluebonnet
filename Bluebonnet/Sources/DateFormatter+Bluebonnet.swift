376bb3cd3c24964922ac02eb86b84e0ef8e73c2c
Andrew Theis <andrewtheis@icloud.com>
2019-03-08 01:15:36 -0600

    Try again

--- Bluebonnet/Sources/DateFormatter+Bluebonnet.swift
+++ Bluebonnet/Sources/DateFormatter+Bluebonnet.swift
@@ -26,26 +26,20 @@ import Foundation
 public extension DateFormatter {
     
     /// For dates in the format of `yyyy-MM-dd'T'HH:mm:ssZ`.
-    public static let iso8601DateTime: DateFormatter = {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssXXXXX"
-        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
-        return dateFormatter
-    }()
+    public static let iso8601DateTime = DateFormatter.iso8601(format: "yyyy-MM-dd'T'HH:mm:ssXXXXX")
     
     /// For dates in the format of `yyyy-MM-dd'T'HH:mm:ss.SSSZ`.
-    public static let iso8601DateTimeWithMilliseconds: DateFormatter = {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSXXXXX"
-        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
-        return dateFormatter
-    }()
+    public static let iso8601DateTimeWithMilliseconds = DateFormatter.iso8601(format: "yyyy-MM-dd'T'HH:mm:ss.SSSXXXXX")
     
     /// For dates in the format of `yyyy-MM-dd'T'HH:mm:ss.SSSSSSZ`.
-    public static let iso8601DateTimeWithMicroseconds: DateFormatter = {
+    public static let iso8601DateTimeWithMicroseconds = DateFormatter.iso8601(format: "yyyy-MM-dd'T'HH:mm:ss.SSSSSSXXXXX")
+    
+    /// Convenience method to create above formats.
+    private static func iso8601(format: String) -> DateFormatter {
         let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSSSSXXXXX"
+        dateFormatter.dateFormat = format
         dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        dateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
         return dateFormatter
-    }()
+    }
 }
